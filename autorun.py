281e775f82ffd31a83b395b703942c00929affcd
campanulamediuml <campanulamediuml@gmail.com>
2017-08-14 15:22:06 +0800

    自动运行脚本

--- autorun.py
+++ autorun.py
@@ -4,36 +4,20 @@ import os
 import time
 import config
 
-
+count = 0
 while True:
-    not_null = []
-    for i in config.keyword:
-        fh = open(i+'asid_list.txt')
-        content = fh.readlines()#获取asin列表，如果asin列表不存在，就先获取
-        if len(content) != 0:
-            not_null.append(i)
-    fh.close()
-    #读取几个文件，看看文件内容，把写完的文件剔除掉，留下还没写完的
+    fh = open('asid_list.txt')
+    answer = fh.readline()
+    if answer[0] == '' or len(answer) == 0:
+        os.system('python get_asin_list.py')
+        count+=1
+        if count == 10:
+            break
+    else:
+        os.system('python commentspider.py')
 
-    dir_name = config.dir_name
-    fh = open('config.py','w')
-    content = '","'.join(not_null)
-    fh.write('keyword = ["'+content+'"]')
-    fh.write('\n')
-    fh.write('dir_name = "'+dir_name+'"'+'\n')
-    fh.write('keyword_db = [""]'+'\n')
-    fh.write('good_type = "'+config.good_type+'"\n')
-    fh.write('pro_website = "'+config.pro_website+'"')
-    fh.close()
-    #更新配置文件内容
-
-    time.sleep(3)
-    os.system('python commentspider.py')
-    
-    fh.close()
-    if dir_name[0] == '':
-        break
 
 os.system('python intodb.py')
-os.system('python dbreset.py')
+os.system('python main_run.py')
+
 
---
+++ washdb.py
@@ -0,0 +1,10 @@
+import MySQLdb as mydatabase
+conn = mydatabase.connect(host='117.25.155.149', port=3306, user='gelinroot', passwd='glt#789A', db='dolphin_staff', charset='utf8')
+cursor = conn.cursor()
+
+cursor.execute('DELETE FROM py_keyword_main')
+cursor.execute('DELETE FROM py_keyword_main_tmp')
+cursor.execute('DELETE FROM py_keyword_word_count')
+cursor.execute('DELETE FROM py_keyword_count')
+
+conn.commit()
