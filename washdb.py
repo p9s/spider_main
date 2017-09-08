a0a1146ceda420a1a04da27d014d78bd3f01fad2
campanulamediuml <campanulamediuml@gmail.com>
2017-09-08 17:27:35 +0800

    注释

--- washdb.py
+++ washdb.py
@@ -2,7 +2,8 @@ import MySQLdb as mydatabase
 conn = mydatabase.connect(host='117.25.155.149', port=3306, user='gelinroot', passwd='glt#789A', db='db_dolphin', charset='utf8')
 cursor = conn.cursor()
 
-cursor.execute('DELETE FROM py_product_comments_tmp')
+#cursor.execute('DELETE FROM py_product_comments_tmp')
+#不能删掉tmp表
 cursor.execute('DELETE FROM py_keyword_main')
 cursor.execute('DELETE FROM py_keyword_main_tmp')
 cursor.execute('DELETE FROM py_keyword_word_count')
