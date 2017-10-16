c3fa643c3a6b083ca33dafc2eec01d3182661a8b
campanulamediuml <campanulamediuml@gmail.com>
2017-10-16 16:44:34 +0800

    添加跨地区

---
+++ french/main.py
@@ -0,0 +1,10 @@
+import time
+import os
+
+
+os.system('python washdb.py')
+os.system('python resetnew.py')
+os.system('python word_key.py')
+os.system('python set_AZCM.py')
+os.system('python word_count_key.py')
+
